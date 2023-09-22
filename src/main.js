c53e685927aed94e62d05d00c23406f5ea4cb030
el1xz <glaiber2010@gmail.com>
2023-09-22 12:31:47 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
